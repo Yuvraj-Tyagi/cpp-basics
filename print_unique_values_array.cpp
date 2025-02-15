d93e8f48f69ae46250b4abf433c4aa3607f5a2cf
Yuvraj-Tyagi <yuvrajtyagi05@gmail.com>
2025-02-16 01:22:48 +0530

    Add files via upload

---
+++ print_unique_values_array.cpp
@@ -0,0 +1,22 @@
+// print all the unique values in an array
+#include <iostream>
+using namespace std;
+
+void print_unique_values(int arr[], int size){
+    for(int i=0;i<size;i++){
+        int count = 0;
+        for (int j =0;j<size;j++){
+            if(arr[i]==arr[j]){
+                count++;
+            }
+        }
+        if(count == 1){
+            cout<<arr[i]<<" ";
+        }
+    }
+}
+
+int main(){
+    int nums[10] = {12,34,56,54,12,67,56,34,88,90};
+    print_unique_values(nums,10);
+}
