21ebee6aaa45b1aef025e58af0435cd6900957f9
Yuvraj-Tyagi <yuvrajtyagi05@gmail.com>
2025-02-18 21:54:11 +0530

    Update arrays.cpp

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -20,5 +20,5 @@ int main(){
 
 // Array indexes go from 0 to size-1
 // Array is always automatically passed by reference
-// Arrays have a fixed size
+// Arrays have a fixed size once their size is declared.
 
